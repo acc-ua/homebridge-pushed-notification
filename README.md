fcc27a253c4f1d094912d84cb565890709169b74
acc-ua <shanyuk@gmail.com>
2017-11-28 01:55:07 +0200

    initial

---
+++ README.md
@@ -0,0 +1,24 @@
+# homebridge-pushed-notification
+Create accessories used to send GetPushed (https://pushed.co) notifications using HomeKit Automation
+
+The Homebridge Notification accessory is a simple switch which sends a notification to GetPushed whenever it is toggled On. The Notification accessory is meant to be used in conjunction with HomeKit Automation. A user can program an automation using the Home app to send a notification whenever an event/state (or combination of) occurs. This is done by programming the Automation to set the Notfication accessory state to On. Currently, the Home application only supports native notification from sensors such as Motion Detectors, Presense Sensors, Door Locks, etc.. This Homebridge accessory bridges the gap and allows any HomeKit state to trigger a notification. For example, you can send a notification if the temperature in your home is below a threshold but the thermostat is currently turned off. 
+
+The Home app provides only very limited functionality for programming Automations. There is however an excellent HomeKit app called "Hesperus" which allows the creation of Actions (if/then) based on any HomeKit device state.
+
+## GetPushed Configuration
+
+### "app_key"
+Your GetPushed APP key. Set the key to empty if you do not want to receive notifications.
+
+### "app_secret"
+The GetPushed APP secret string.
+
+### "app_name"
+The name of GetPushed application
+
+### "message"
+The message which is to be sent to IFTTT if the accessory is toggled On. 
+
+### "mute_notification_interval_in_sec"
+This value specifies how long notification should be muted after a trigger. This is to prevent notification flooding if the Automation which triggered the notification is repeated.
+
---
+++ index.js
@@ -0,0 +1,123 @@
+var request = require('request');
+
+var Service;
+var Characteristic;
+
+module.exports = function (homebridge)
+{
+  Service = homebridge.hap.Service;
+  Characteristic = homebridge.hap.Characteristic;
+  homebridge.registerAccessory("homebridge-pushed-notifications", "GetPushedNotification", PushedNotificationAccessory);
+}
+
+function PushedNotificationAccessory(log, config)
+{
+  this.log = log;
+  this.accessoryLabel = config['accessory'];
+  this.name = config['name'];
+
+  this.serviceAppKey = config['app_key'];
+  this.serviceAppName = config['app_name'];
+  this.serviceAppSecret = config['app_secret'];
+  this.notificationMessage = config['message'];
+  this.muteNotificationIntervalInSec = config['mute_notification_interval_in_sec'];
+
+  this.log(" app key " + this.serviceAppKey);
+  this.log(" event " + this.serviceAppName);
+  this.log(" message " + this.message);
+  this.log(" mute notification interval in sec " + this.muteNotificationIntervalInSec);
+
+  this.serviceMuted = false;
+  this.stateValue = 0;
+
+  this.SendNotification = function()
+  {
+    this.log('Send notification to GetPushed: ' + this.notificationMessage);
+
+    const data = {
+      'app_key': this.serviceAppKey,
+      'app_secret': this.serviceAppSecret,
+      'target_type': "app",
+      'content': this.notificationMessage
+    };
+
+    request.post({url:'https://api.pushed.co/1/push', form: data}, function(err,httpResponse,body)
+    { 
+
+    });
+
+  }
+}
+
+
+PushedNotificationAccessory.prototype =
+{
+
+  getValue: function(callback) {
+        var that = this;
+        callback(null, false);
+  },
+
+  setValue: function (value, callback)
+  {
+    this.log('setState ' + value);
+    this.stateValue = value;
+
+    if (this.stateValue == 1) {
+      // 'that' is used inside timeout functions
+      that = this;
+
+      // Clear the On value after 250 milliseconds 
+      setTimeout(function() {that.stateValue = 0; that.btnService.setCharacteristic(Characteristic.On, 0) }, 500 );
+
+      // Send GetPushed notification
+      if (this.serviceAppKey) {
+         if (this.serviceMuted == false) {
+           this.SendNotification();
+
+           // Mute further notifications for specified time
+           this.serviceMuted = true;
+           setTimeout(function() {that.serviceMuted = false; that.log("GetPushed un-muted");}, this.muteNotificationIntervalInSec * 1000);
+         }
+         else {
+           this.log("GetPushed notification is muted");
+         }
+       }
+    }
+    callback(null);
+  },
+
+  identify: function (callback)
+  {
+    this.log("Identify requested!");
+    callback();
+  },
+
+  getServices: function ()
+  {
+
+        var services = [];
+        var infoService = new Service.AccessoryInformation();
+        infoService
+            .setCharacteristic(Characteristic.Manufacturer, "Acc-UA")
+            .setCharacteristic(Characteristic.Model, "GetPushedNotificationButton")
+            .setCharacteristic(Characteristic.SerialNumber, "GetPushedNotificationButton2017");
+        services.push(infoService);
+
+        this.btnService = new Service.Switch(this.name);
+         this.btnService
+            .getCharacteristic(Characteristic.On)
+            .on('get', this.getValue.bind(this))
+            .on('set', this.setValue.bind(this));
+        services.push( this.btnService);
+
+        return services;
+
+
+  }
+
+  
+
+   
+
+};
